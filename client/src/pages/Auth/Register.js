ae178b281804f29fe0ecc3a345e3b9ac522cda0c
Suraj-harry <suraj18316@gmail.com>
2026-02-02 14:22:55 +0000

    part8.1

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Layout from "./../../components/Layout/Layout";
+import Layout from "../../components/Layout/Layout";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
@@ -18,7 +18,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/register", {
+      const res = await axios.post("/api/v1/auth/Register", {
         name,
         email,
         password,
