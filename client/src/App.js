7a4b8d2bfec09981d29c351b353d81ec291875a6
Suraj-harry <suraj18316@gmail.com>
2026-02-02 13:45:04 +0000

    part6

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import {Routes , Route} from 'react-router-dom'
+import Homepage from './pages/Homepage.js'
+import About from './pages/About.js'
+import Contact from './pages/Contact.js'
+import Policy from './pages/Policy.js'
+import Pagenotfound from './pages/Pagenotfound.js'
+import Register from './pages/Auth/Register.js'
+import { ToastContainer } from 'react-toastify';
+import Login from './pages/Auth/Login.js'
+import DashBoard from './pages/user/DashBoard.js'
+import PrivateRoute from './components/Routes/Private.js'
+import ForgotPassword from './pages/Auth/ForgotPassword.js'
+import AdminDashboard from './pages/Admin/AdminDashboard.js'
+import AdminRoute from './components/Routes/AdminRoute.js'
+import CreateCategory from './pages/Admin/CreateCategory.js'
+import CreateProduct from './pages/Admin/CreateProduct.js'
+import Users from './pages/Admin/Users.js'
+import Orders from './pages/user/Orders.js'
+import Profile from './pages/user/Profile.js'
+import Products from './pages/Admin/Products.js'
+import UpdateProduct from './pages/Admin/UpdateProduct.js'
 
 function App() {
   return (
     <>
-      <h1>E-commerce Web</h1>
+      <Routes>
+          <Route  path='/' element={<Homepage/>}/>
+          <Route path='/dashboard' element={<PrivateRoute/>}>
+            <Route  path='user' element={<DashBoard/>}/>
+            <Route  path='user/orders' element={<Orders/>}/>
+            <Route  path='user/profile' element={<Profile/>}/>
+          </Route>
+          <Route path='/dashboard' element ={<AdminRoute/>}>
+            <Route path='admin' element = {<AdminDashboard/>}/>
+            <Route path='admin/create-category' element = {<CreateCategory/>}/>
+            <Route path='admin/create-product' element = {<CreateProduct/>}/>
+            <Route path='admin/product/:slug' element = {<UpdateProduct/>}/>  
+            <Route path='admin/products' element = {<Products/>}/> 
+            <Route path='admin/users' element = {<Users/>}/>           
+          </Route>
+          <Route  path='/register' element={<Register/>}/>
+          <Route  path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route  path='/login' element={<Login/>}/>
+          <Route  path='/about' element={<About/>}/>
+          <Route  path='/contact' element={<Contact/>}/>
+          <Route  path='/policy' element={<Policy/>}/>
+          <Route  path='*' element={<Pagenotfound/>}/>
+      </Routes>
     </>
   );
 }
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout title={'Privacy Policy of - Ecommerce App'}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/image/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>Compliance policy</p>
+          <p>Framework  policy</p>
+          <p>Placeholder policy</p>
+          <p>Testing policy</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "TechInfo",
+};
+
+export default Policy;
